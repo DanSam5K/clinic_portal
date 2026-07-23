ec8b0eac30347047f5ebfcaee0d95e077806ab74
Daniel Samuel <dansam5000@gmail.com>
2026-07-23 11:53:28 +0200

    adding image as context

--- app/(public)/preview/page.tsx
+++ app/(public)/preview/page.tsx
@@ -1,9 +1,16 @@
 // preview page for newly created UI components
 
+import Skeleton from "@/components/Skeleton"
+
 export default function PreviewPage() {
   return (
     <div className="page-content">
       <h2>Preview</h2>
+      <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+      </div>
     </div>
   )
 }
